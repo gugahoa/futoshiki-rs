a8204e1f450cc509dd190c02eccdc7cc6d286ab6
Gustavo Aguiar <gugahoa@gmail.com>
2016-10-02 02:10:13 -0300

    Create Matrix struct and primitive functions
    Matrix struct has rows and cols members, as well as data that stores the
    value of a specific cell and the function to call if it is to be
    modified

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+struct Matrix {
+    rows: u32,
+    cols: u32,
+    data: Vec<(u32, Box<(Fn(u32) -> i8)>)>,
+}
+
+impl Matrix {
+    fn new(rows: u32, cols: u32) -> Matrix {
+        Matrix {
+            rows: rows,
+            cols: cols,
+            data: Vec::new(),
+        }
+    }
+
+    fn get(&self, x: u32, y: u32) -> Option<&(u32, Box<(Fn(u32) -> i8)>)> {
+        if x >= self.rows {
+            return None;
+        }
+
+        if y >= self.cols {
+            return None;
+        }
+
+        Some(&self.data[(x + y * self.rows) as usize])
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut m = Matrix::new(4, 4);
+    for i in 0..16 {
+        m.data.push((i as u32,
+                     Box::new(|index: u32| -> i8 {
+            if index == 12 {
+                -1
+            } else {
+                0
+            }
+        })));
+    }
+
+    print!("{}", m.get(3, 3).unwrap().0);
 }
